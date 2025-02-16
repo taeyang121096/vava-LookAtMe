3e48920239d967d4b4fb461e18aad5af4119bc78
taeyang121096 <taeyang121096@naver.com>
2025-02-16 15:21:01 +0900

    querydsl config

--- lookAtMe/build.gradle.kts
+++ lookAtMe/build.gradle.kts
@@ -1,10 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.25"
-//	kotlin("kapt") version "1.9.25"
+	kotlin("kapt") version "1.9.25"
 }
 
 group = "com.vava"
@@ -27,7 +29,6 @@ repositories {
 }
 
 dependencies {
-
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -35,14 +36,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	implementation("com.querydsl:querydsl-jpa:5.0.0")
-	implementation("com.querydsl:querydsl-core:5.0.0")
-
 	implementation("com.h2database:h2")
 
-//	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
-//	kapt("jakarta.persistence:jakarta.persistence-api:3.1.0")
-//	kapt("jakarta.annotation:jakarta.annotation-api:2.1.1")
+	implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+	kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+	kapt("jakarta.persistence:jakarta.persistence-api:3.1.0")
+	kapt("jakarta.annotation:jakarta.annotation-api:2.1.1")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
@@ -55,19 +54,40 @@ dependencies {
 }
 
 kotlin {
+	jvmToolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
 }
 
-//kapt {
-//	arguments {
-//		arg("querydsl.generatedSourcesDir", layout.buildDirectory.dir("generated/querydsl").get().asFile.absolutePath)
-//	}
-//}
+val generated = file("src/main/generated")
+
+tasks.withType<JavaCompile> {
+	options.generatedSourceOutputDirectory.set(generated)
+}
 
 sourceSets.main {
-	java.srcDirs(layout.buildDirectory.dir("generated/querydsl"))
+	kotlin.srcDirs += generated
+}
+
+tasks.named("clean") {
+	doLast {
+		generated.deleteRecursively()
+	}
+}
+
+kapt {
+	generateStubs = true
+}
+
+
+
+noArg {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
 }
 
 allOpen {
---
+++ lookAtMe/src/main/kotlin/com/vava/lookAtMe/config/QueryDslConfig.kt
@@ -0,0 +1,17 @@
+package com.vava.lookAtMe.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig(
+    private val em: EntityManager
+) {
+
+    @Bean
+    fun queryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(em)
+    }
+}
